52e6ddcbbe48be33ed041839198f8f3e56a48fc8
renanfortkamp <93234608+renanfortkamp@users.noreply.github.com>
2021-10-29 15:49:40 -0300

    inversão do alert idadeDepois ou Antes

--- modulo-01/ex08/script/script.js
+++ modulo-01/ex08/script/script.js
@@ -6,12 +6,12 @@ Em seguida, exiba na tela um “alert” contendo o texto “Sua idade é x ou y
 
 var anoNascimento = parseInt(prompt("Digite o ano de seu nascimento!"));
 var anoAtual = parseInt(prompt("Digite o ano atual!"));
-var idadeAntes = anoAtual - anoNascimento;
-var idadeDepois = anoAtual - anoNascimento +1;
+var idadeAntes = anoAtual - anoNascimento -1;
+var idadeDepois = anoAtual - anoNascimento;
 
 const alerta = () => {
     if(anoNascimento >= 0 && anoNascimento < anoAtual && anoAtual > anoNascimento && anoAtual < 9999 ){
-    alert(`Sua idade é ${idadeAntes} ou ${idadeDepois}`)
+    alert(`Sua idade é ${idadeDepois} ou ${idadeAntes}`)
     } else {alert("Ano Atual ou Ano de Nascimento digitado incorretamente, recarregue a pagina e tenta novamente!")}
 
 } 
