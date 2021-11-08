311e434a66ee27557a52a6ecd843170b8ad56643
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2021-11-08 00:40:51 -0300

    ex01 - trocando metodo data

--- modulo-01/semana3/ex01/script.js
+++ modulo-01/semana3/ex01/script.js
@@ -4,18 +4,19 @@ const calcularIdade = ()=>{
     let dia = dataAtual.getDate();
     let mes = dataAtual.getMonth() + 1;
     let ano = dataAtual.getFullYear();
-    let dataNascimento = document.getElementById('dataNascimento').value;
-    console.log(dataNascimento)
-    let anoNascimento = parseInt(dataNascimento.slice(0,4));
-    let mesNascimento = parseInt(dataNascimento.slice(5,7));
-    let diaNascimento = parseInt(dataNascimento.slice(8,10));
+    let dataNascimento = document.getElementById('dataNascimento').value.split("-");
+    let anoNascimento = Number(dataNascimento[0]);
+    let mesNascimento = Number(dataNascimento[1]);
+    let diaNascimento = Number(dataNascimento[2]);
+
+
     if(isNaN(anoNascimento) || isNaN(mesNascimento) || isNaN(diaNascimento)){
         alert("Você não inseriu sua data de nascimento, insira para obter sua idade");
     }else{
-        if(dia > diaNascimento && mes >= mesNascimento && ano >= anoNascimento || dia < diaNascimento && mes > mesNascimento && ano >= anoNascimento){
+        if(dia >= diaNascimento && mes >= mesNascimento && ano > anoNascimento || dia < diaNascimento && mes > mesNascimento && ano > anoNascimento){
             let idade = ano - anoNascimento;
             document.querySelector("h2").innerText = idade;
-        } else if(diaNascimento >= dia && mesNascimento >= mes && anoNascimento >= anoNascimento || diaNascimento < dia && mesNascimento > mes && anoNascimento >= ano){
+        } else if(diaNascimento > dia && mesNascimento >= mes && anoNascimento >= anoNascimento || diaNascimento < dia && mesNascimento > mes && anoNascimento >= ano){
             alert("Insira a data corretamente!")
         } else{
             let idade = ano - anoNascimento - 1;
