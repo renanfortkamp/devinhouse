2af013fc97868e44a9095a6466c07555c82bc57f
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2021-11-16 21:47:58 -0300

    adicionado função para armazenar

---
+++ modulo-01/semana3/ex08/readme.md
@@ -0,0 +1,1 @@
+Utilizando a mesma página do exercício anterior (exercício 7), adicione um novo botão “Salvar lista”. Quando o usuário clicar nesse botão, a lista de itens de mercado deve ser salva no Local Storage do navegador.
---
+++ modulo-01/semana3/ex08/script.js
@@ -0,0 +1,33 @@
+/* Sua página deve possibilitar ao usuário inserir itens em uma lista e exibir a lista atualizada à medida que forem sendo adicionados novos itens.
+
+A página deve conter um rótulo (<label>) “Item a adicionar:” seguido de um campo de texto onde o usuário possa digitar o item. Deve conter também um botão “Adicionar”, que no momento do clique do usuário deve inserir o item digitado na lista. Se o texto estiver vazio (length), deve mostrar uma mensagem ao usuário solicitando a inserção de algum texto.
+
+A página também deve conter uma lista (<select>), que exibirá os itens adicionados pelo usuário. */
+
+var lista = new Array();
+
+
+const valid = ()=>{
+    let item = document.querySelector('input').value;
+    if( item.length == 0){
+        alert("Digite o item a ser adicionado na lista!")
+    } else{
+        addItem()
+    }
+
+
+}
+const addItem = ()=>{
+    let item = document.querySelector('input').value;
+    lista.push(item);
+    for(i=0;i<lista.length;i++){
+    let opt = document.createElement('option');
+    opt.innerHTML = lista[i];
+    document.querySelector('select').appendChild(opt);
+    }
+    item = document.querySelector('input').value = "";
+}
+
+const saveList = () =>{
+    JSON.stringify(localStorage.setItem('lista', lista))
+}
