f3a5189a8cd7aac2ab9ab76478638537c5674a12
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2022-02-20 20:41:17 -0300

    Ex 01 e 02 da segunda semana do modulo 02

---
+++ modulo-02/ExSemana02/ex01/ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿// [M2S02] Ex 1 - Calcular o raio de um círculo
+
+namespace Ex01{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a área do circulo para calcular seu raio!");
+            var areaString = Console.ReadLine();
+            var areaInteiro = Convert.ToInt32(areaString);
+            var raio = Math.PI * (areaInteiro * 2);
+            Console.WriteLine($"O Raio do circulo é {raio}²");
+
+        }
+    }
+}
---
+++ modulo-02/ExSemana02/ex02/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a temperadutra em celcius para convertela para fahrenheit!");
+            var celciusString = Console.ReadLine();
+            int celciusInt = Convert.ToInt32(celciusString);
+            var fahrenheit = ((celciusInt * 1.8) + 32);
+            Console.WriteLine($"Sua temperatura em Fahrenheit é {fahrenheit}F");
+        }
+    }
+}
