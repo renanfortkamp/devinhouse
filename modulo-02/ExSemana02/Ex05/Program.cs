624873146fe06268b3284c5080d203c6f505d43c
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2022-02-20 21:49:15 -0300

    Ex05 da semana 02 do modulo 02

---
+++ modulo-02/ExSemana02/Ex05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace Ex05
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ArrayList arr = new ArrayList(10);
+            arr = new ArrayList { };
+            do
+            {
+                Console.WriteLine("Digite um numero");
+                var numString = Console.ReadLine();
+                if (numString == "")
+                {
+                    Console.WriteLine("Você não digitou um numero valido!");
+                }
+                else
+                {
+                    int num = Convert.ToInt32(numString);
+                    arr.Add(num);
+
+                }
+
+
+            } while (arr.Count < 10);
+            Console.WriteLine("Lista dos numeros digitados antes do filtro decrescente");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            arr.Sort();
+            arr.Reverse();
+            Console.WriteLine("Lista dos numeros digitados apos filtro decrescente");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
