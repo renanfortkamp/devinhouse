af2e5d0bb7e1241f7cced01907a90fbbd77c102c
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2022-02-20 21:11:03 -0300

    Ex04 da semana 02 do modulo 02

---
+++ modulo-02/ExSemana02/Ex04/Program.cs
@@ -0,0 +1,31 @@
+﻿// [M2S02] Ex 3 - Busca número menor
+
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  numeroMenor= 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Digite um numero");
+                var numString = Console.ReadLine();
+                if (numString == "")
+                {
+                    Console.WriteLine("Voce não digitou um numero valido!");
+                }
+                else
+                {
+                    int num = Convert.ToInt32(numString);
+                    if (num < numeroMenor)
+                    {
+                        numeroMenor = num;
+                    }
+                }
+            }
+            Console.WriteLine($"O Menor numero digitado é {numeroMenor}");
+        }
+    }
+}
