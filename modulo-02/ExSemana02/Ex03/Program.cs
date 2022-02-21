37020bee47acbc1fb5fff390421cf56afa0fd364
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2022-02-20 21:01:53 -0300

    Ex 03 da semana 02 do modulo 02

---
+++ modulo-02/ExSemana02/Ex03/Program.cs
@@ -0,0 +1,32 @@
+﻿// [M2S02] Ex 3 - Busca número maior
+
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroMaior = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite um numero");
+                var numString = Console.ReadLine();
+                if (numString == "")
+                {
+                    Console.WriteLine("Voce não digitou um numero!");
+                }
+                else
+                {
+                    int num = Convert.ToInt32(numString);
+                    if (num > numeroMaior)
+                    {
+                        numeroMaior = num;
+                    }
+                }
+            }
+            Console.WriteLine($"O Maior numero digitado é {numeroMaior}");
+        }
+    }
+}
+
