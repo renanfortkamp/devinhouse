244486fb4ee179faf0d4af6050e2f52ecfd1f244
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2022-02-20 22:36:13 -0300

    Ex07 da semana 02

---
+++ modulo-02/ExSemana02/Ex07/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro");
+            var numString = Console.ReadLine();
+            if (numString != null)
+            {
+                var numInvertido = new string(numString.Reverse().ToArray());
+                int numInt = Convert.ToInt32(numInvertido);
+                Console.WriteLine(numInt);
+            }
+            
+        }
+    }
+}
+
